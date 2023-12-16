e8d55688be1d6ad12b1f30b7239d93e47229dd50
StarrFox <StarrFox6312@gmail.com>
2023-12-16 03:15:29 +0000

    reset cooldown for all non-cooldown errors
    and provide a better message for the user

--- arcanumbot/extensions/error_handler.py
+++ arcanumbot/extensions/error_handler.py
@@ -1,14 +1,19 @@
 import logging
 from datetime import timedelta
+import discord
 
 from discord.ext import commands
 from humanize import naturaldelta
 
+from arcanumbot import ArcanumBot
+
 logger = logging.getLogger(__name__)
 
 
-async def on_command_error(ctx: commands.Context, error):
+async def on_command_error(ctx: commands.Context[ArcanumBot], error):
     error = getattr(error, "original", error)
+
+    cooldowns_reset = False
 
     if isinstance(error, commands.CommandNotFound):
         return
@@ -22,13 +27,14 @@ async def on_command_error(ctx: commands.Context, error):
         return await ctx.send("You don't have permission to run that command")
 
     # Reset cooldown when command doesn't finish
-    elif isinstance(error, commands.CommandError) and not isinstance(
+    elif not isinstance(
         error, commands.CommandOnCooldown
     ):
         if ctx.command is not None:
             ctx.command.reset_cooldown(ctx)
 
-        return await ctx.send(str(error))
+            cooldowns_reset = True
+
 
     elif isinstance(error, commands.CommandOnCooldown):
         delta = timedelta(seconds=error.retry_after)
@@ -41,12 +47,19 @@ async def on_command_error(ctx: commands.Context, error):
             f"Unhandled error in command {ctx.command.name} Invoke message: {ctx.message.content} {error=}"
         )
 
-    await ctx.send(f"Unknown error while executing {ctx.command}, will be fixed soon")
+    cooldown_message = "; cooldown reset" if cooldowns_reset else ""
+
+    owner_mention = f"<@{ctx.bot.owner_id}> " if ctx.bot.owner_id is not None else ""
+
+    await ctx.send(
+        f"{owner_mention}Unknown error while executing {ctx.command}{cooldown_message}",
+        allowed_mentions=discord.AllowedMentions(users=True)
+    )
 
 
-async def setup(bot: commands.Bot):
+async def setup(bot: ArcanumBot):
     bot.add_listener(on_command_error)
 
 
-async def teardown(bot: commands.Bot):
+async def teardown(bot: ArcanumBot):
     bot.remove_listener(on_command_error)
