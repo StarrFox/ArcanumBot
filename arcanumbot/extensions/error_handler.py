1183557d4742f93018f884e545c4559856873937
StarrFox <StarrFox6312@gmail.com>
2023-12-17 14:50:07 +0000

    fix error handler logic

--- arcanumbot/extensions/error_handler.py
+++ arcanumbot/extensions/error_handler.py
@@ -13,43 +13,39 @@ logger = logging.getLogger(__name__)
 async def on_command_error(ctx: commands.Context[ArcanumBot], error):
     error = getattr(error, "original", error)
 
-    cooldowns_reset = False
-
     if isinstance(error, commands.CommandNotFound):
         return
 
     # Bypass checks for owner
-    elif isinstance(error, commands.CheckFailure):
+    if isinstance(error, commands.CheckFailure):
         if await ctx.bot.is_owner(ctx.author):
             await ctx.reinvoke()
             return
 
         return await ctx.send("You don't have permission to run that command")
 
-    # Reset cooldown when command doesn't finish
-    elif not isinstance(error, commands.CommandOnCooldown):
-        if ctx.command is not None:
-            ctx.command.reset_cooldown(ctx)
-
-            cooldowns_reset = True
-
-    elif isinstance(error, commands.CommandOnCooldown):
+    if isinstance(error, commands.CommandOnCooldown):
         delta = timedelta(seconds=error.retry_after)
         natural = naturaldelta(delta)
         # TODO: use pendulum for this so we can drop the humanize dependency
         return await ctx.send(f"Command on cooldown, retry in {natural}.")
+    
+    # Reset cooldown when command doesn't finish
+    else:
+        if ctx.command is not None:
+            ctx.command.reset_cooldown(ctx)
+            cooldown_message = "; cooldown reset"
+        
+        else:
+            cooldown_message = ""
 
     if ctx.command is not None:
         logger.error(
             f"Unhandled error in command {ctx.command.name} Invoke message: {ctx.message.content} {error=}"
         )
 
-    cooldown_message = "; cooldown reset" if cooldowns_reset else ""
-
-    owner_mention = f"<@{ctx.bot.owner_id}> " if ctx.bot.owner_id is not None else ""
-
     await ctx.send(
-        f"{owner_mention}Unknown error while executing {ctx.command}{cooldown_message}",
+        f"Unknown error while executing {ctx.command}{cooldown_message}",
         allowed_mentions=discord.AllowedMentions(users=True),
     )
 
